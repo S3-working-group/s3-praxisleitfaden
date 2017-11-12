e9c52d37796bc227edc00adf9cb571e443e1dbe8
bboc <bernhard.bockelbrink@gmail.com>
2017-11-12 19:45:03 +0100

    Build-Skripte aktualisiert

--- build-slides.sh
+++ build-slides.sh
@@ -1,15 +1,14 @@
 
 # build deckset
-mdslides compile s3-practical-guide.yaml src/ tmp/ --chapter-title=img
-mdslides build deckset s3-practical-guide.yaml tmp/ S3-Praxisleitfaden.md --template=templates/deckset-template.md
+mdslides compile s3-practical-guide.yaml src/ tmp/ --chapter-title=img --glossary=glossary.yaml
+mdslides build deckset s3-practical-guide.yaml tmp/ S3-Praxisleitfaden.md --template=templates/deckset-template.md  --glossary=glossary.yaml --glossary-items=16
 # append pattern-index
-python build_index.py >>S3-Praxisleitfaden.md
-
+python pattern_index_deckset.py S3-Praxisleitfaden.md pattern-index.yaml
 
 # build reveal.js
-mdslides compile s3-practical-guide.yaml src/ tmp/ --chapter-title=text
-mdslides build revealjs s3-practical-guide.yaml  tmp/ reveal.js/S3-Praxisleitfaden.html --template=templates/revealjs-template.html
+mdslides compile s3-practical-guide.yaml src/ tmp/ --chapter-title=text --glossary=glossary.yaml
+mdslides build revealjs s3-practical-guide.yaml  tmp/ reveal.js/S3-Praxisleitfaden.html --template=templates/revealjs-template.html  --glossary=glossary.yaml  --glossary-items=8
 
 # build wordpress output
-mdslides compile s3-practical-guide.yaml src/ tmp/ --chapter-title=none
-mdslides build wordpress s3-practical-guide.yaml tmp/ web-out/ --footer=templates/wordpress-footer.md
+mdslides compile s3-practical-guide.yaml src/ tmp/ --chapter-title=none --glossary=glossary.yaml
+mdslides build wordpress s3-practical-guide.yaml tmp/ web-out/ --footer=templates/wordpress-footer.md  --glossary=glossary.yaml
